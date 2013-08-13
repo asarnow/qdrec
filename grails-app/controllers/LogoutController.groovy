cb430402b8a5d71e1e0f19105742975c2da16d70
Daniel Asarnow <dasarnow@sfsu.edu>
2013-08-12 20:18:37 -0700

    Use save state service method

--- grails-app/controllers/LogoutController.groovy
+++ grails-app/controllers/LogoutController.groovy
@@ -1,26 +1,14 @@
-import edu.sfsu.ntd.phenometrainer.Parasite
-import edu.sfsu.ntd.phenometrainer.ParasiteTrainState
-import edu.sfsu.ntd.phenometrainer.Users
 import org.codehaus.groovy.grails.plugins.springsecurity.SpringSecurityUtils
 
 class LogoutController {
-  def springSecurityService
+//  def springSecurityService
+  def trainService
 	/**
 	 * Index action. Redirects to the Spring security logout uri.
 	 */
 	def index = {
 
-    def user = (Users)springSecurityService.getCurrentUser()
-
-    session["parasites"].each { k,v ->
-      def parasite = Parasite.findById(v.id)
-      def parasiteTrainState = ParasiteTrainState.findByTrainerAndParasite(user,parasite)
-      if (parasiteTrainState==null) parasiteTrainState = new ParasiteTrainState()
-      parasiteTrainState.parasite = parasite
-      parasiteTrainState.trainState = v.trainState
-      parasiteTrainState.trainer = user
-      parasiteTrainState.save(flush: true)
-    }
+    trainService.saveCurrentImageState(session["parasites"])
 
 		redirect uri: SpringSecurityUtils.securityConfig.logout.filterProcessesUrl // '/j_spring_security_logout'
 	}
