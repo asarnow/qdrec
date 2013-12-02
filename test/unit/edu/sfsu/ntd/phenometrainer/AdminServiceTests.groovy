483fd95d995045cca4e09302256df4afdb8567d6
Daniel Asarnow <dasarnow@gmail.com>
2013-12-02 01:33:25 -0800

    new tests

---
+++ test/unit/edu/sfsu/ntd/phenometrainer/AdminServiceTests.groovy
@@ -0,0 +1,17 @@
+package edu.sfsu.ntd.phenometrainer
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(AdminService)
+class AdminServiceTests {
+
+  void testSomething() {
+    fail "Implement me"
+  }
+}
---
+++ test/unit/edu/sfsu/ntd/phenometrainer/ClassifyServiceTests.groovy
@@ -0,0 +1,17 @@
+package edu.sfsu.ntd.phenometrainer
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(ClassifyService)
+class ClassifyServiceTests {
+
+  void testSomething() {
+    fail "Implement me"
+  }
+}
