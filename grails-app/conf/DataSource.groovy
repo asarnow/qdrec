fada3359aa9fc5bd3bc971fd52b823919e443b29
Daniel Asarnow <dasarnow@gmail.com>
2013-12-03 04:24:34 -0800

    use new db, create-drop

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -18,9 +18,9 @@ environments {
         dataSource {
             username = "root"
             password = ""
-            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
 //            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            url = "jdbc:mysql://localhost/phenome_trainer?useUnicode=yes&characterEncoding=UTF-8"
+            url = "jdbc:mysql://localhost/phenome_trainer2?useUnicode=yes&characterEncoding=UTF-8"
         }
     }
     test {
