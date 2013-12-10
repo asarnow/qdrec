7591cf5bc633a7baf11c8d2fe099024881ba985e
Daniel Asarnow <dasarnow@sfsu.edu>
2013-12-09 20:05:36 -0800

    View and action for testing output layout

--- grails-app/controllers/edu/sfsu/ntd/phenometrainer/ClassifyController.groovy
+++ grails-app/controllers/edu/sfsu/ntd/phenometrainer/ClassifyController.groovy
@@ -27,6 +27,47 @@ class ClassifyController {
     }
   }
 
+  def testClassify() {
+    double[][] cm = [ [87.0, 3.0], [0.0, 76.0] ]
+
+    double[][] Rtrain = [[0.04, 27.0],
+                         [0.04, 25.0],
+                         [0.13, 32.0],
+                         [0.7,  30.0],
+                         [1.0,  29.0],
+                         [1.0,  23.0] ]
+
+    double[][] Rtest = [[0.03, 31.0],
+                        [0.0,	 23.0],
+                        [0.13, 31.0],
+                        [0.75, 24.0],
+                        [0.97, 30.0],
+                        [1.0,	 18.0] ]
+
+    def trainImages = ["072913-CTRL-0-4-b",
+                       "072913-NIC-0001-4-b",
+                       "072913-NIC-001-4-b",
+                       "072913-NIC-01-4-b",
+                       "072913-NIC-1-4-b",
+                       "072913-NIC-10-4-b"]
+
+    def testImages = ["072913-CTRL-0-4-b",
+                      "072913-NIC-0001-4-b",
+                      "072913-NIC-001-4-b",
+                      "072913-NIC-01-4-b",
+                      "072913-NIC-1-4-b",
+                      "072913-NIC-10-4-b"]
+
+    render(view: 'testClassify',
+                  model: [cm: cm as double[][],
+                          Rtrain: Rtrain as double[][],
+                          Rtest: Rtest as double[][],
+                          trainImages: trainImages as List,
+                          testImages: testImages as List,
+                          datasetID: params.datasetID,
+                          subsets: Dataset.get(params.datasetID).subsets])
+  }
+
   def downloadResults() {
 
   }
