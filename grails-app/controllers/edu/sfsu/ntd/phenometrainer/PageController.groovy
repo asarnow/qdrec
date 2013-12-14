d0da7c5a5a491ba1e0246554a51e066cba3f5642
Daniel Asarnow <dasarnow@gmail.com>
2013-12-13 21:37:46 -0800

    Save parasites agro

--- grails-app/controllers/edu/sfsu/ntd/phenometrainer/PageController.groovy
+++ grails-app/controllers/edu/sfsu/ntd/phenometrainer/PageController.groovy
@@ -2,11 +2,15 @@ package edu.sfsu.ntd.phenometrainer
 
 class PageController {
 
+    def trainService
+
     def about() {
+      trainService.saveCurrentImageState(session["parasites"])
       render(view: 'about')
     }
 
     def help() {
+      trainService.saveCurrentImageState(session["parasites"])
       render(view:'help')
     }
 
