e272d2d2d59d2cc0d446eeb2211426e791dae360
Daniel Asarnow <dasarnow@gmail.com>
2013-12-13 22:51:50 -0800

    per env log4j, log to files

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -70,6 +70,34 @@ environments {
           dataDir = "/home/da/Documents/IDEAProjects/PhenomeTrainer/app-data"
           svmsFile = "/home/da/Documents/IDEAProjects/PhenomeTrainer/app-data/svms.mat"
         }
+
+      // log4j configuration
+      log4j = {
+          // Example of changing the log pattern for the default console appender:
+          //
+          appenders {
+          //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
+            file name: 'file', file:'/tmp/qdrec.log'
+          }
+          root {
+            debug 'stout', 'file'
+            additivity = true
+          }
+
+          error  'org.codehaus.groovy.grails.web.servlet',        // controllers
+                 'org.codehaus.groovy.grails.web.pages',          // GSP
+                 'org.codehaus.groovy.grails.web.sitemesh',       // layouts
+                 'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+                 'org.codehaus.groovy.grails.web.mapping',        // URL mapping
+                 'org.codehaus.groovy.grails.commons',            // core / classloading
+                 'org.codehaus.groovy.grails.plugins',            // plugins
+                 'org.codehaus.groovy.grails.orm.hibernate',      // hibernate integration
+                 'org.springframework',
+                 'org.hibernate',
+                 'net.sf.ehcache.hibernate'
+
+          info   'grails.app'
+      }
     }
     production {
         grails.logging.jul.usebridge = false
@@ -81,30 +109,36 @@ environments {
           dataDir = "/home/dev/qdrec-data"
           svmsFile = "/home/dev/qdrec-data/svms.mat"
         }
+
+        // log4j configuration
+        log4j = {
+            // Example of changing the log pattern for the default console appender:
+            //
+            appenders {
+            //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
+              file name: 'file', file:'/home/dev/tomcat6/logs/qdrec.log'
+            }
+            root {
+              error 'file'
+              additivity = true
+            }
+
+            error  'org.codehaus.groovy.grails.web.servlet',        // controllers
+                   'org.codehaus.groovy.grails.web.pages',          // GSP
+                   'org.codehaus.groovy.grails.web.sitemesh',       // layouts
+                   'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+                   'org.codehaus.groovy.grails.web.mapping',        // URL mapping
+                   'org.codehaus.groovy.grails.commons',            // core / classloading
+                   'org.codehaus.groovy.grails.plugins',            // plugins
+                   'org.codehaus.groovy.grails.orm.hibernate',      // hibernate integration
+                   'org.springframework',
+                   'org.hibernate',
+                   'net.sf.ehcache.hibernate'
+
+            info   'grails.app'
+        }
+
     }
-}
-
-// log4j configuration
-log4j = {
-    // Example of changing the log pattern for the default console appender:
-    //
-    //appenders {
-    //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
-    //}
-
-    error  'org.codehaus.groovy.grails.web.servlet',        // controllers
-           'org.codehaus.groovy.grails.web.pages',          // GSP
-           'org.codehaus.groovy.grails.web.sitemesh',       // layouts
-           'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
-           'org.codehaus.groovy.grails.web.mapping',        // URL mapping
-           'org.codehaus.groovy.grails.commons',            // core / classloading
-           'org.codehaus.groovy.grails.plugins',            // plugins
-           'org.codehaus.groovy.grails.orm.hibernate',      // hibernate integration
-           'org.springframework',
-           'org.hibernate',
-           'net.sf.ehcache.hibernate'
-
-    info   'grails.app'
 }
 
 // Added by the Spring Security Core plugin:
