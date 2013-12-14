d7b385abef5313bb87d908becbb897a51d882f0f
Daniel Asarnow <dasarnow@gmail.com>
2013-12-13 22:51:58 -0800

    correct prod ds

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -34,7 +34,7 @@ environments {
             username = "root"
             password = ""
             dbCreate = "validate"
-            url = "jdbc:mysql://localhost/phenome_trainer?useUnicode=yes&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull"
+            url = "jdbc:mysql://localhost/phenome_trainer2?useUnicode=yes&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull"
             pooled = true
             properties {
                maxActive = -1
